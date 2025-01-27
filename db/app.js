f8b78aed8b0a8dddad01e9300239ae8e87e8b102
guidopro <169035608+guidopro@users.noreply.github.com>
2025-01-27 12:24:05 +0000

    creates controllers.js and requires getAllTopics from it into app.js

--- db/app.js
+++ db/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const endpointsJson = require("../endpoints.json");
+const { getAllTopics } = require("./controllers/controllers");
 
 app.get("/api", (req, res) => {
   res.status(200).send({ endpoints: endpointsJson });
 });
+
+app.get("/api/topics", getAllTopics);
 
 module.exports = app;
