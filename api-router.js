a79bf046b5b5acf1526c5f85b0542f4fbcb16859
guidopro <169035608+guidopro@users.noreply.github.com>
2025-02-03 22:32:01 +0000

    makes changes to routing

--- api-router.js
+++ api-router.js
@@ -1,5 +1,10 @@
 const apiRouter = require("express").Router();
 const articlesRouter = require("./articles-router");
+const {
+  getAllTopics,
+  deleteComment,
+  getAllUsers,
+} = require("./controllers/controllers");
 const endpointsJson = require("./endpoints.json");
 
 apiRouter.use("/articles", articlesRouter);
@@ -7,5 +12,9 @@ apiRouter.use("/articles", articlesRouter);
 apiRouter.get("/", (req, res) => {
   res.status(200).send({ endpoints: endpointsJson });
 });
+
+apiRouter.get("/topics", getAllTopics);
+apiRouter.delete("/comments/:comment_id", deleteComment);
+apiRouter.get("/users", getAllUsers);
 
 module.exports = apiRouter;
--- articles-router.js
+++ articles-router.js
@@ -2,11 +2,17 @@ const {
   getArticleById,
   getAllArticles,
   getCommentsByArticleId,
+  patchArticle,
+  postCommentByArticleId,
 } = require("./controllers/controllers");
 const articlesRouter = require("express").Router();
 
 articlesRouter.get("/", getAllArticles);
-articlesRouter.get("/:article_id", getArticleById);
-articlesRouter.get("/:article_id/comments", getCommentsByArticleId);
+articlesRouter.route("/:article_id").get(getArticleById).patch(patchArticle);
+
+articlesRouter
+  .route("/:article_id/comments")
+  .get(getCommentsByArticleId)
+  .post(postCommentByArticleId);
 
 module.exports = articlesRouter;
